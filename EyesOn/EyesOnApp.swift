7eebfacb0d782ee880a076ccfb693066634abfe9
Avital <avital12210v@gmail.com>
2025-06-22 21:35:47 +0300

    Initial Commit

---
+++ EyesOn/EyesOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyesOnApp.swift
+//  EyesOn
+//
+//  Created by Avital on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct EyesOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EyesOnTests/EyesOnTests.swift
@@ -0,0 +1,17 @@
+//
+//  EyesOnTests.swift
+//  EyesOnTests
+//
+//  Created by Avital on 22/06/2025.
+//
+
+import Testing
+@testable import EyesOn
+
+struct EyesOnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
