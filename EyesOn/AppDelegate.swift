f86234707cf5fc26e409534124f3f5bd435bb089
Avital <avital12210v@gmail.com>
2025-06-26 00:34:25 +0300

    added- notification(Alert) when new report approved

--- EyesOn/AppDelegate.swift
+++ EyesOn/AppDelegate.swift
@@ -2,21 +2,19 @@
 //  AppDelegate.swift
 //  EyesOn
 //
-//  Created by Avital on 23/06/2025.
-//
 
 import UIKit
 import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
-        // Configure Firebase
-        FirebaseApp.configure()
+        // Firebase should already be configured in App init, but double-check
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+        
+        print("Firebase app configured: \(FirebaseApp.app() != nil)")
         
         return true
     }
